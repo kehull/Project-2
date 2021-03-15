408b43a4df1969b16e555eddb422186667609422
Janelle <jmbnw9@gmail.com>
2021-03-15 07:57:52 -0500

    working on project

--- clean_data_df.sql
+++ clean_data_df.sql
@@ -1,10 +1,10 @@
 CREATE TABLE "clean_data_df" (
     "incident_name" VARCHAR   NOT NULL,
     "county" VARCHAR   NOT NULL,
-    "acres_burned" FLOAT DEFAULT NULL,
-    "longitude" FLOAT DEFAULT NULL,
-    "latitude" FLOAT DEFAULT NULL,
+    "acres_burned" int   NOT NULL,
+    "longitude" int   NOT NULL,
+    "latitude" int   NOT NULL,
     "incident_type" VARCHAR   NOT NULL,
-    "date_extinguished" FLOAT DEFAULT NULL,
-    "date_created" FLOAT DEFAULT NULL
+    "date_extinguished" date   NOT NULL,
+    "date_created" date   NOT NULL
 );
