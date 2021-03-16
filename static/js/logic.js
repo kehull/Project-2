db03401cda3ec169f08226bd17e94d4837713c93
Ava Wiegert <aemily6296@gmail.com>
2021-03-16 18:18:48 -0500

    added county boundary data

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,19 +1,8 @@
 // Creating map object
 var myMap = L.map("mapid", {
-  center: [40.7, -73.95],
+  center: [36.7783, -119.4179],
   zoom: 11
 });
-
-// Adding tile layer to the map
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  tileSize: 512,
-  maxZoom: 18,
-  zoomOffset: -1,
-  id: "mapbox/streets-v11",
-  accessToken: API_KEY
-}).addTo(myMap);
-
 
 // Adding tile layer
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
@@ -26,7 +15,7 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 }).addTo(myMap);
 
 // Use this link to get the geojson data.
-var link = "https://services6.arcgis.com/snwvZ3EmaoXJiugR/arcgis/rest/services/BOE_citycounty/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json";
+var link = "data/City_and_County_Boundaries.geojson";
 
 // Grabbing our GeoJSON data..
 d3.json(link, function(data) {
