d1d1d22419c40bc7f955fb17b9a1423f29da7388
Ava Wiegert <aemily6296@gmail.com>
2021-03-15 19:49:57 -0500

    EarthquakeData.py is now creating the dataframe correctly

--- static/py/EarthquakeData.py
+++ static/py/EarthquakeData.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 
 # Target data
-startdate = "2013-01-01"
+startdate = "2021-01-01"
 enddate = "2021-03-12"
 minmagnitude = "5"
 minlatitude = "32.30"
@@ -14,14 +14,12 @@ maxlatitude = "42"
 maxlongitude = "-124.24"
 
 # Build the endpoint URL
-target_url = ('https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime={0}&endtime={1}&minmagnitude={2}&minlatitude={3}&minlongitude={4}&maxlatitude={5}&maxlongitude={6}').format(startdate, enddate, minmagnitude, minlatitude, minlongitude, maxlatitude, maxlongitude)
-# target_url = ('https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime={0}&endtime={1}&minmagnitude={2}').format(startdate, enddate, minmagnitude)
+# target_url = ('https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime={0}&endtime={1}&minmagnitude={2}&minlatitude={3}'
+#     '&minlongitude={4}&maxlatitude={5}&maxlongitude={6}').format(startdate, enddate, minmagnitude, minlatitude, minlongitude, maxlatitude, maxlongitude)
+target_url = ('https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime={0}&endtime={1}&minmagnitude={2}').format(startdate, enddate, minmagnitude)
 
 # Request the data
 geo_data = requests.get(target_url).json()
-
-# # Print the json
-# print(json.dumps(geo_data, indent=4, sort_keys=True))
 
 # build columns of dataframe
 place = []
@@ -32,8 +30,8 @@ depth = []
 
 # populate columns
 for response in geo_data['features']:
-    place.append(response['place'])
-    mag.append(response['mag'])
+    place.append(response['properties']['place'])
+    mag.append(response['properties']['mag'])
     lat.append(response['geometry']['coordinates'][1])
     lon.append(response['geometry']['coordinates'][0])
     depth.append(response['geometry']['coordinates'][2])
