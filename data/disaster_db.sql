71aca35c1d588097e4290d31ae7469ce2d888f70
kehull <71107280+kehull@users.noreply.github.com>
2021-03-18 20:20:51 -0500

    sql that works but only with absolute paths

--- data/disaster_db.sql
+++ data/disaster_db.sql
@@ -1,5 +1,6 @@
+DROP TABLE IF EXISTS WILDFIRES;
+
 CREATE TABLE IF NOT EXISTS WILDFIRES (
-	id NUMERIC UNIQUE NOT NULL,
 	incident_name VARCHAR,
 	county VARCHAR,
 	acres_burned NUMERIC,
@@ -8,38 +9,41 @@ CREATE TABLE IF NOT EXISTS WILDFIRES (
 	incident_type VARCHAR,
 	date_extinguished VARCHAR,
 	date_created VARCHAR,
-	PRIMARY KEY(id)
+	PRIMARY KEY(incident_name, date_created)
 );
 
 COPY WILDFIRES(incident_name, county, acres_burned, longitude, latitude, incident_type, date_extinguished, date_created)
-FROM 'clean_fire.csv'
+FROM 'C:\Users\kelly\Documents\Completed_Homework\Project-2\data\clean_fire.csv'
 DELIMITER ','
 CSV HEADER;
 
+DROP TABLE IF EXISTS EARTHQUAKES; 
+
 CREATE TABLE IF NOT EXISTS EARTHQUAKES (
-	id NUMERIC UNIQUE NOT NULL,
 	place VARCHAR,
 	time NUMERIC,
 	magnitude NUMERIC,
 	latitude NUMERIC,
+	longitude NUMERIC,
 	depth NUMERIC,
-	PRIMARY KEY(id)
+	PRIMARY KEY(place, time)
 );
 
-COPY EARTHQUAKES(place, time, magnitude, latitude, depth)
-FROM 'California_EarthQuake.csv'
+COPY EARTHQUAKES(place, time, magnitude, latitude, longitude, depth)
+FROM 'C:\Users\kelly\Documents\Completed_Homework\Project-2\data\California_EarthQuake.csv'
 DELIMITER ','
 CSV HEADER;
 
+DROP TABLE IF EXISTS COUNTIES;
+
 CREATE TABLE IF NOT EXISTS COUNTIES (
-	id NUMERIC UNIQUE NOT NULL,
 	county VARCHAR,
 	city VARCHAR,
 	polygon VARCHAR,
-	PRIMARY KEY(id)
+	PRIMARY KEY(county,city)
 );
 
 COPY COUNTIES(county, city, polygon)
-FROM 'County_Boundaries.csv'
+FROM 'C:\Users\kelly\Documents\Completed_Homework\Project-2\data\County_Boundaries.csv'
 DELIMITER ','
 CSV HEADER;
