1e6f6a1967455a6374b72819edd092efd22a4852
Ava Wiegert <aemily6296@gmail.com>
2021-03-20 14:00:03 -0500

    re-format html, started filter logic

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,3 +1,4 @@
+// CREATE COUNTIES MAP ______________________________________________________________________________
 // Creating map object
 var myMap = L.map("mapid", {
   center: [36.7783, -119.4179],
@@ -22,3 +23,93 @@ d3.json(link, function(data) {
   // Creating a GeoJSON layer with the retrieved data
   L.geoJson(data).addTo(myMap);
 });
+
+
+// FILTER FUNCTIONS ______________________________________________________________________________
+// get current date
+function getToday() {
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, '0');
+  var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  var yyyy = today.getFullYear();
+  today = mm + '/' + dd + '/' + yyyy;
+
+//   var date = new Date(dateTime.getTime());
+//   date.setHours(0, 0, 0, 0);
+  return today;
+}
+
+// find the button (id in HTML is filter-btn)
+var button = d3.select("#filter");
+
+// find the form (id in HTML is form-group)
+var form = d3.select("#form-group");
+
+// grab full data sets
+var fireData = insertAPIcallhere
+var earthquakeData = insertAPIcallhere
+
+// set filtered data to default values
+var filteredFire = fireData // add default values
+var filteredEarthquake = earthquakeData // add default values
+
+// create filter function for datasets
+function filterData() {
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // Select the input element and get the raw HTML node
+  var inputElement_startdate = d3.select("#datetime");
+  var inputElement_enddate = d3.select("#city");
+  var inputElement_excludeFire = d3.select("#state");
+  var inputElement_excludeEarthquake = d3.select("#country");
+
+  // Get the value property of the input element
+  var inputValue_start = inputElement_startdate.property("value");
+  var inputValue_end = inputElement_enddate.property("value");
+  var inputValue_fire = inputElement_excludeFire.property("value");
+  var inputValue_earchquake = inputElement_excludeEarthquake.property("value");
+
+  var filteredFire = fireData
+  var filteredEarthquake = earthquakeData
+
+  if (inputValue_start !== null && inputValue_start !== '') {
+    filteredFire = filteredFire.filter( data => data.datetime > inputValue_date);
+    filteredEarthquake = filteredEarthquake.filter( data => data.datetime > inputValue_date);
+  }
+  else {
+    filteredFire = filteredFire.filter( data => data.datetime > '01/01/2013');
+    filteredEarthquake = filteredEarthquake.filter( data => data.datetime > '01/01/2013');
+  };
+
+  if (inputValue_end !== null && inputValue_end !== '') {
+    filteredFire = filteredFire.filter( data => data.datetime < inputValue_date);
+    filteredEarthquake = filteredEarthquake.filter( data => data.datetime < inputValue_date);
+  }
+  else {
+    filteredFire = filteredFire.filter( data => data.datetime < getToday());
+    filteredEarthquake = filteredEarthquake.filter( data => data.datetime < getToday());
+  };
+
+  if (inputValue_fire == null && inputValue_fire == '') {
+    filteredFire = filteredFire.filter( data => data.datetime < '01/01/2013'); //find a better way to set the dataset to null
+  };
+
+  if (inputValue_earchquake == null && inputValue_earchquake == '') {
+    filteredEarthquake = filteredEarthquake.filter( data => data.datetime < '01/01/2013'); //find a better way to set the dataset to null
+  };
+
+
+  return filteredFire, filteredEarthquake
+};
+
+// DANGER POINTS FUNCTION _______________________________________________________________________
+
+
+// CREATE FIRE MAP ______________________________________________________________________________
+
+
+// CREATE EARTHQUAKE MAP ________________________________________________________________________
+
+
+// CREATE BAR CHART _____________________________________________________________________________
