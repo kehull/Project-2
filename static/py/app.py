308aba38ad8bb6f21750a6ccef130e8de056729f
David Fournie <drfournie@gmail.com>
2021-03-22 01:38:48 -0500

    finished api

--- static/py/app.py
+++ static/py/app.py
@@ -9,84 +9,75 @@ from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
 
 #database setup
-path="../../data/wildfires.sqlite"
+path="../../data/californiadisasters.sqlite"
 engine=create_engine(f"sqlite:///{path}")
-Base = automap_base()
-Base.prepare(engine,reflect=True)
-results_test=engine.execute("SELECT * FROM WILDFIRES")
 
-
-
-
-# results_test_list_2=[]
-# results_test_2=engine.execute("SELECT * FROM WILDFIRES WHERE date_created >= '12/4/2017'")
-# for result in results_test_2:
-#     results_test_list_2.append(result)
-# print(results_test_list_2)
-
-
-# earthquake=Base.classes.earthquake
+#create dates list for checking on for fire api's
 dates_list=[]
 dates=engine.execute("SELECT date_created FROM WILDFIRES")
 for date in dates:
     dates_list.append(date[0])
 
-# print(dates_list)
-
- 
+#create counties list for checking on for fire api's
 county_list=[]
 counties=engine.execute("SELECT county FROM WILDFIRES")
 for county in counties:
     county_list.append(county[0])
 
+#create depth list to check max for earthquake api
+depth_list=[]
+depth=engine.execute("SELECT depth FROM EARTHQUAKES")
+for km in depth:
+    depth_list.append(float(km[0]))
 
+#create flask app
 app=Flask(__name__)
+#ignore key sort
 app.config['JSON_SORT_KEYS'] = False
 
 # set 'Home' route 
 @app.route("/")
-#create function that displays instructions for the pathways
+#create function that tells the server when user has entered home page
 def welcome():
     print("Server has recieved request for 'Welcome' page...")
     
-        # Available routes fire test
-        # f"/api/v1.0/fire"  returns entire dataset for 
-        # f"/api/v1.0/fire/COUNTY<br/>" returns specific county
-        # f"/api/v1.0/fire/incident/BOOLEAN" returns fires that are over or still going
-        # f"/api/v1.0/fire/date/START<br/>" returns fires that started on and after date given
-        # f"/api/v1.0/fire/date/START/END" returns  date range entered
-    
+        # Available routes fire 
+        # /api/v1.0/fire  returns entire dataset for fire db
+        # /api/v1.0/fire/county/<county>  returns specific county
+        # /api/v1.0/fire/date/<date>  returns fires that started on date given
+        # /api/v1.0/fire/classification/<classification>  returns fires based on class
 
-#set precipitation route
+        # Available routes earthquake
+        # /api/v1.0/earthquake  returns entire dataset for earthquake db
+        # /api/v1.0/earthquake/magnitude/<magnitude>  returns earthquakes that are >= magnitude given
+        # /api/v1.0/earthquake/depth/<depth> returns  earthquakes that are >= given depth
+        # /api/v1.0/earthquake/classification/magnitude/classification/<classification>  returns earthquakes based on magnitude class
+        #/api/v1.0/earthquake/classification/depth/classification/<classification>  returns earthquakes based on depth class
+    
+    return f"error! Please input proper api path",404
+
+#set fire db route
 @app.route("/api/v1.0/fire")
-# create function that returns precipitation results for query
+
+# create function that returns fire db results for query
 def entire_db():
     
-    
-    
-    #filter query by station with most measurments
+    #create results list to store results from sqlalchemy
     results_list=[]
+    #create query
     results=engine.execute("SELECT * FROM WILDFIRES;")
+    #loop through query and append to results list
     for result in results:
         results_list.append(result)
     
-    
-    
-     #close session for efficiency   
-    
-    
-    
-    #create dictionary that uses date as key and precipitation as value
+    #create dictionary to hold data for api
     json_dict={"data":[]}
 
-    
-   
-    
-    
+    #loop through results list
     for name,county,acres_burned,lng,lat,kind,date_extinguished,date_created in results_list:
-
+        #create test_dict to store info for each loop
         test_dict={"properties":{}}
-    
+        #populate test_dict
         test_dict["properties"]["name"]=name
         test_dict["properties"]["county"]=county
         test_dict["properties"]["acres_burned"]=acres_burned
@@ -95,28 +86,27 @@ def entire_db():
         test_dict["properties"]["date_ext"]=date_extinguished
         test_dict["properties"]["lat"]=lat
         test_dict["properties"]["lng"]=lng
+        #add each instance of test_dict to json_dict
         json_dict["data"].append(test_dict)
         
-
-
-
-        
-        
-        
-    #     #return jsonified dictionary
+    #return jsonified dictionary
     return jsonify(json_dict)
 
-@app.route("/api/v1.0/fire/<county>")
+#repeat mostly for county
+@app.route("/api/v1.0/fire/county/<county>")
 def location(county):
-    canonicalization=f'{county}'
+    #create a string version of argument
+    test=f'{county}'
+    #create var that changes inpuit to title
+    canonicalization=test.title()
     
-    if county in county_list:
+    if canonicalization in county_list:
         results_list=[]
         results=engine.execute(f"SELECT * FROM WILDFIRES WHERE county LIKE'{canonicalization}%'")
         
         for result in results:
             results_list.append(result)
-        print(results_list)
+        
         json_dict={"data":[]}
      
     
@@ -134,32 +124,25 @@ def location(county):
             test_dict["properties"]["lng"]=lng
             json_dict["data"].append(test_dict)
 
-
-
-        
-        
-        
         #return jsonified dictionary
         return jsonify(json_dict)
     else: 
-        f"error! {county} not found",404
+        f"error! {county} county not found",404
 
-#set route for start date only
-@app.route("/api/v1.0/fire/date/<start>")
-def one_date(start):
-    canonicalization=f'{start}'
+#repeat for date
+@app.route("/api/v1.0/fire/date/<date>")
+def one_date(date):
+    canonicalization=f'{date}'
     
-    
-    if start in dates_list:
+    if date in dates_list:
         results_list=[]
-        results=engine.execute(f"SELECT * FROM WILDFIRES WHERE date_created >= '{canonicalization}'")
+        results=engine.execute(f"SELECT * FROM WILDFIRES WHERE date_created == '{canonicalization}'")
         
         for result in results:
             results_list.append(result)
-        print(results_list)
+        
         json_dict={"data":[]}
      
-    
         for name,county,acres_burned,lng,lat,kind,date_extinguished,date_created in results_list:
 
             test_dict={"properties":{}}
@@ -172,75 +155,220 @@ def one_date(start):
             test_dict["properties"]["date_ext"]=date_extinguished
             test_dict["properties"]["lat"]=lat
             test_dict["properties"]["lng"]=lng
+            
             json_dict["data"].append(test_dict)
 
-
-
-        
-        
-        
-        #return jsonified dictionary
+        #return json_dict
         return jsonify(json_dict)
     else: 
-        f"error! {start} not found",404
+        f"error! {date} date not found",404
 
-
-
-# #repeat procoss of start date to date range
-# @app.route("/api/v1.0/fire/date<start>/<end>")
-# def date_range(start=None,end=None):
+#repeat for fire classification
+@app.route("/api/v1.0/fire/classification/<classification>")
+def fire_class(classification):
+    test=f'{classification}'
+    canonicalization=test.title()
+    results_list=[]
+    #create blank results that change value depending on conditional
+    results=""
+    #conditional
+    if canonicalization == "G":
+        results=engine.execute("SELECT * FROM WILDFIRES WHERE acres_burned >= 5000 ORDER BY acres_burned")
+    elif canonicalization =="F":
+        results=engine.execute("SELECT * FROM WILDFIRES WHERE acres_burned >= 1000 AND acres_burned <5000 ORDER BY acres_burned")
+    elif canonicalization =="E":
+        results=engine.execute("SELECT * FROM WILDFIRES WHERE acres_burned >= 300 AND acres_burned <1000 ORDER BY acres_burned")
+    elif canonicalization =="D":
+        results=engine.execute("SELECT * FROM WILDFIRES WHERE acres_burned >= 100 AND acres_burned <300 ORDER BY acres_burned")
+    elif canonicalization =="C":
+        results=engine.execute("SELECT * FROM WILDFIRES WHERE acres_burned >= 10 AND acres_burned <100 ORDER BY acres_burned")
+    elif canonicalization =="B":
+        results=engine.execute("SELECT * FROM WILDFIRES WHERE acres_burned > .25 AND acres_burned <10 ORDER BY acres_burned")
+    elif canonicalization =="A":
+        results=engine.execute("SELECT * FROM WILDFIRES WHERE acres_burned <=.25 ORDER BY acres_burned")
+    else:
+        return f"error! {canonicalization} classification not found."
     
-#     if start in dates_list and end in dates_list and end > start:
-#         session=Session(engine)
-#         results_2=session.query(fire).\
-#         filter(fire.incident_dateonly_created >= start).\
-#         filter(fire.incident_dateonly_extinguished<= end).all()
-    
-#         session.close()
-#         json_list=[]
+    for result in results:
+            results_list.append(result)
+        
+    json_dict={"data":[]}
      
+    for name,county,acres_burned,lng,lat,kind,date_extinguished,date_created in results_list:
+        test_dict={"properties":{}}
+        
+        test_dict["properties"]["name"]=name
+        test_dict["properties"]["county"]=county
+        test_dict["properties"]["acres_burned"]=acres_burned
+        test_dict["properties"]["type"]=kind
+        test_dict["properties"]["date_cre"]=date_created
+        test_dict["properties"]["date_ext"]=date_extinguished
+        test_dict["properties"]["lat"]=lat
+        test_dict["properties"]["lng"]=lng
+        
+        json_dict["data"].append(test_dict)
+
+    #return jsonified dictionary
+    return jsonify(json_dict)
+
+
+
+#repeat for earthquake db
+@app.route("/api/v1.0/earthquake")
+def earthquake_db():
+    results_list=[]
+    results=engine.execute("SELECT * FROM EARTHQUAKES")
     
-#         for name, ongoing, update, date,unit,unit_url,county,location,acres,containment, control, agency,lng, lat, kind,id,url,extinguished, date_ext,dat_cre,active,cal_inc,noti in results_2:
-#             features_dict={}
-#             coordinates_dict={}   
-#             features_dict["name"]=name
-#             features_dict["ongoing"]=ongoing
-#             features_dict["last_update"]=update
-#             features_dict["date_created"]=date
-#             features_dict["unit"]=unit
-#             features_dict["unit_url"]=unit_url
-#             features_dict["county"]=county
-#             features_dict["location"]=location
-#             features_dict["acres_burned"]=acres
-#             features_dict["containment"]=containment
-#             features_dict["incident_control"]=control
-#             features_dict["agency"]=agency
-#             features_dict["type"]=kind
-#             features_dict["id"]=id
-#             features_dict["url"]=url
-#             features_dict["ext_datetime"]=extinguished
-#             features_dict["ext_date"]=date_ext
-#             features_dict["date_created"]=dat_cre
-#             features_dict["active"]=active
-#             features_dict["calfire"]=cal_inc
-#             features_dict["notification_desired"]=noti
-#             coordinates_dict["lat"]=lat
-#             coordinates_dict["lng"]=lng     
-
-#             json_list.append(features_dict)
-#             json_list.append(coordinates_dict)
-
-
-
+    for result in results:
+        results_list.append(result)
+    
+    json_dict={"data":[]}
+    
+    for place, time, mag, lng,lat,depth in results_list:
+        test_dict={"properties":{}}
         
+        test_dict["properties"]["location"]=place
+        test_dict["properties"]["epoch_time"]=time
+        test_dict["properties"]["magnitude"]=mag
+        test_dict["properties"]["latitude"]=lat
+        test_dict["properties"]["longitude"]=lng
+        test_dict["properties"]["depth(km)"]=depth
         
+        json_dict["data"].append(test_dict)
+
+    return jsonify(json_dict)
+
+#repeat for earthquake magnitude
+@app.route("/api/v1.0/earthquake/magnitude/<magnitude>")
+def magnitude_return(magnitude):
+    canonicalization= int(magnitude)
+    if canonicalization <=10:
+        results_list=[]
+        results=engine.execute(f"SELECT * FROM EARTHQUAKES WHERE magnitude >={canonicalization} ORDER BY magnitude")
         
-#         #return jsonified dictionary
-#         return jsonify(json_list)
-#     else: 
-#         f"error! Range {start} : {end} not found",404
+        for result in results:
+            results_list.append(result)
 
+        json_dict={"data":[]}
+        
+        for place, time, mag, lng,lat,depth in results_list:
+            test_dict={"properties":{}}
+            
+            test_dict["properties"]["location"]=place
+            test_dict["properties"]["epoch_time"]=time
+            test_dict["properties"]["magnitude"]=mag
+            test_dict["properties"]["latitude"]=lat
+            test_dict["properties"]["longitude"]=lng
+            test_dict["properties"]["depth(km)"]=depth
+            
+            json_dict["data"].append(test_dict)
+        
+        return jsonify(json_dict)
+    else:
+        return f"error! {magnitude} magnitude not found."
+#repeat for depth
+@app.route("/api/v1.0/earthquake/depth/<depth>") 
+def depth_return(depth):
+    canonicalization=int(depth)
+    if canonicalization <= max(depth_list):
+        results_list=[]
+        results=engine.execute(f"SELECT * FROM EARTHQUAKES WHERE depth >= {canonicalization} ORDER BY depth")
+        
+        for result in results:
+            results_list.append(result)
 
+        json_dict={"data":[]}
+        
+        for place, time, mag, lng,lat,depth in results_list:
+            test_dict={"properties":{}}
+            
+            test_dict["properties"]["location"]=place
+            test_dict["properties"]["epoch_time"]=time
+            test_dict["properties"]["magnitude"]=mag
+            test_dict["properties"]["latitude"]=lat
+            test_dict["properties"]["longitude"]=lng
+            test_dict["properties"]["depth(km)"]=depth
+            
+            json_dict["data"].append(test_dict)
+        
+        return jsonify(json_dict)
+    else:
+        return f"error! {depth} depth not found."
+
+#repeat for magnitude classification
+@app.route("/api/v1.0/earthquake/classification/magnitude/classification/<classification>")
+def mag_class(classification):
+    test=f'{classification}'
+    canonicalization=test.title()
+    results_list=[]
+    results=""
+    
+    if canonicalization == "Great":
+        results=engine.execute("SELECT * FROM EARTHQUAKES WHERE magnitude >=8 ORDER BY magnitude")
+    elif canonicalization =="Major":
+        results=engine.execute("SELECT * FROM EARTHQUAKES WHERE magnitude >=7 AND magnitude < 8 ORDER BY magnitude")
+    elif canonicalization =="Strong":
+        results=engine.execute("SELECT * FROM EARTHQUAKES WHERE magnitude >=6 AND magnitude < 7 ORDER BY magnitude")
+    elif canonicalization =="Moderate":
+        results=engine.execute("SELECT * FROM EARTHQUAKES WHERE magnitude < 6 ORDER BY magnitude")
+    else:
+        return f"error! {canonicalization} classification not found."
+    
+    for result in results:
+            results_list.append(result)
+
+    json_dict={"data":[]}
+    
+    for place, time, mag, lng,lat,depth in results_list:
+        
+        test_dict={"properties":{}}
+        test_dict["properties"]["location"]=place
+        test_dict["properties"]["epoch_time"]=time
+        test_dict["properties"]["magnitude"]=mag
+        test_dict["properties"]["latitude"]=lat
+        test_dict["properties"]["longitude"]=lng
+        test_dict["properties"]["depth(km)"]=depth
+        
+        json_dict["data"].append(test_dict)
+        
+    return jsonify(json_dict)
+
+#repeat for depth classification 
+@app.route("/api/v1.0/earthquake/classification/depth/classification/<classification>")
+def depth_class(classification):
+    test=f"{classification}"
+    canonicalization=test.title()
+    results_list=[]
+    results=""
+    
+    if canonicalization == "Deep":
+        results=engine.execute("SELECT * FROM EARTHQUAKES WHERE depth >= 33 ORDER BY depth")
+    elif canonicalization =="Mid":
+        results=engine.execute("SELECT * FROM EARTHQUAKES WHERE depth >= 17 AND depth <33 ORDER BY depth")
+    elif canonicalization =="Shallow":
+        results=engine.execute("SELECT * FROM EARTHQUAKES WHERE depth < 17 ORDER BY depth")
+    else:
+        return f"error! {canonicalization} classification not found."
+    
+    for result in results:
+            results_list.append(result)
+    
+    json_dict={"data":[]}
+    
+    for place, time, mag, lng,lat,depth in results_list:
+        
+        test_dict={"properties":{}}
+        test_dict["properties"]["location"]=place
+        test_dict["properties"]["epoch_time"]=time
+        test_dict["properties"]["magnitude"]=mag
+        test_dict["properties"]["latitude"]=lat
+        test_dict["properties"]["longitude"]=lng
+        test_dict["properties"]["depth(km)"]=depth
+        
+        json_dict["data"].append(test_dict)
+        
+    return jsonify(json_dict)
+    
 #close out flask
 if __name__=='__main__':
     app.run(debug=True)
